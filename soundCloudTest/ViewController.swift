b20a78d8876e0cb1c9455e895feba8cbf51694cb
thanhhvt <thanhhvt7196@gmail.com>
2020-04-24 17:41:22 +0700

    Initial Commit

---
+++ soundCloudTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  soundCloudTest
+//
+//  Created by kennyS on 4/24/20.
+//  Copyright © 2020 kennyS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
